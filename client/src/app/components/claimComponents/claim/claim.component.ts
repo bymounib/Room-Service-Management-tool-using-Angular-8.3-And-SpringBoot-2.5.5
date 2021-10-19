f71cbe404ca5235f946933be745eb36c39077bff
bymounib <bymounib1997@gmail.com>
2021-10-19 21:59:14 +0100

    set claim.ts methods

--- client/src/app/components/claimComponents/claim/claim.component.ts
+++ client/src/app/components/claimComponents/claim/claim.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ClaimsService } from "src/app/services/claims/claims.service";
+import { Observable, interval, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-claim',
-  templateUrl: './claim.component.html',
-  styleUrls: ['./claim.component.css']
+  selector: "app-claim",
+  templateUrl: "./claim.component.html",
+  styleUrls: ["./claim.component.css"],
 })
 export class ClaimComponent implements OnInit {
+  claims;
+  private updateSubscription: Subscription;
 
-  constructor() { }
-
+  constructor(private claimsList: ClaimsService) {}
   ngOnInit() {
-  }
+    this.claimsList.getClaims().subscribe((data) => {
+      this.claims = data;
+      console.log(this.claims);
+    });
 
+    this.updateSubscription = interval(1000).subscribe(() => {
+      this.claimsList.getClaims().subscribe((data) => {
+        this.claims = data;
+      });
+    });
+  }
+  deleteClaim(claim) {
+    this.claimsList.deleteClaim(claim);
+  }
 }
