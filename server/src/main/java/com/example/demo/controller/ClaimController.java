36e294baf28f033165cdc65ec0cdb6430fa8f675
bymounib <bymounib1997@gmail.com>
2021-10-19 22:00:06 +0100

    add @CrossOrigin to ClaimController

--- server/src/main/java/com/example/demo/controller/ClaimController.java
+++ server/src/main/java/com/example/demo/controller/ClaimController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.Claim;
 import com.example.demo.service.ClaimService;
 
+
+
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 public class ClaimController {
 
