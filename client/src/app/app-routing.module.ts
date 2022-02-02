7c50414017a7fd0bf99ca35cb4afc5f1a4aa34cf
bymounib <bymounib1997@gmail.com>
2022-02-02 21:13:06 +0100

    Update app-routing-module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ClaimComponent } from "./components/claimComponents/claim/claim.component";
+import { AuthGuard } from "./components/guards/auth.guard";
 import { OrderComponent } from "./components/orderComponents/order/order.component";
 
 const routes: Routes = [
   {
-    path: "",
-    component: OrderComponent,
+    path: "/order",
+    component: OrderComponent, canActivate: [AuthGuard]
   },
-  { path: "claim", component: ClaimComponent },
+  { path: "claim", component: ClaimComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
