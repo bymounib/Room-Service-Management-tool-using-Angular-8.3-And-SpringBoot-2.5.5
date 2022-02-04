0ccada9315cfbaa70316b36706d3d6c28503a7cf
bymounib <bymounib1997@gmail.com>
2022-02-04 16:33:18 +0100

    Update UserController

--- server/src/main/java/com/example/demo/controller/UserController.java
+++ server/src/main/java/com/example/demo/controller/UserController.java
@@ -1,21 +1,63 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.AuthenticationRequest;
+import com.example.demo.model.AuthenticationResponse;
+import com.example.demo.service.MyUserDetailsService;
+import com.example.demo.util.JwtUtil;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 
-public class UserController {
+class UserController {
 
-    @GetMapping("/home")
-    public String home(){
-        return "This is Home Page";
-    }
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
-    @GetMapping("/admin")
-    public String admin(){
-        return "This is Admin Page";
-    }
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@RequestMapping({ "/hello" })
+	public String firstPage() {
+		return "Hello World";
+	}
+
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+			);
+		}
+		catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+
+		final UserDetails userDetails = userDetailsService
+				.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+	}
+
 }
