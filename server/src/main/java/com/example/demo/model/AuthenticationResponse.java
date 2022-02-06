2f9f21abbe7e649113333c12e88c8a202986612d
bymounib <bymounib1997@gmail.com>
2022-02-06 11:30:42 +0100

    Update AuthenticationResponse - add id and name to login response

--- server/src/main/java/com/example/demo/model/AuthenticationResponse.java
+++ server/src/main/java/com/example/demo/model/AuthenticationResponse.java
@@ -4,13 +4,26 @@ import java.io.Serializable;
 
 public class AuthenticationResponse implements Serializable {
 
+	private final Integer id;
     private final String jwt;
-
-    public AuthenticationResponse(String jwt) {
+    private final String nom;
+    
+    public AuthenticationResponse(Integer id, String nom,String jwt) {
+        this.id=id;
+    	this.nom=nom;
         this.jwt = jwt;
     }
 
     public String getJwt() {
         return jwt;
     }
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
 }
