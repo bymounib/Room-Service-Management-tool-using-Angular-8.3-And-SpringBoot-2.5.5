cae69a4d8513254dc9a3550908fad8be9fe6f17d
bymounib <bymounib1997@gmail.com>
2022-02-09 16:51:40 +0100

    add @EnableSwagger2

--- server/src/main/java/com/example/demo/ServerApplication.java
+++ server/src/main/java/com/example/demo/ServerApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ServerApplication {
 
 	public static void main(String[] args) {
