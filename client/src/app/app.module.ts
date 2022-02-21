fe2b08ac3e76c51b0955217ac61d0de89a51bad5
bymounib <bymounib1997@gmail.com>
2022-02-21 22:08:49 +0100

    update declarations in app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,30 +5,14 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { SidebarComponent } from "./components/sidebar/sidebar.component";
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { OrderComponent } from "./components/orderComponents/order/order.component";
-import { AddOrderComponent } from "./components/orderComponents/add-order/add-order.component";
-import { PizzaComponent } from "./components/orderComponents/menu/pizza/pizza.component";
-import { SandwichComponent } from "./components/orderComponents/menu/sandwich/sandwich.component";
-import { DrinkComponent } from "./components/orderComponents/menu/drink/drink.component";
-import { ClaimComponent } from "./components/claimComponents/claim/claim.component";
-import { AddClaimComponent } from "./components/claimComponents/add-claim/add-claim.component";
 import { HttpClientModule } from "@angular/common/http";
 import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/notFoundComponent/not-found/not-found.component';
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    NavbarComponent,
-    OrderComponent,
-    AddOrderComponent,
-    PizzaComponent,
-    SandwichComponent,
-    DrinkComponent,
-    ClaimComponent,
-    AddClaimComponent,
     LoginComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
