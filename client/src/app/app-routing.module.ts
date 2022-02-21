0a0157c361379b9f8aa3cbe22d17c11eb9ca9e75
bymounib <bymounib1997@gmail.com>
2022-02-21 21:55:29 +0100

    add notFoundComponent/add lazyLoading principal

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { ClaimComponent } from "./components/claimComponents/claim/claim.component";
-import { AuthGuard } from "./components/guards/auth.guard";
-import { OrderComponent } from "./components/orderComponents/order/order.component";
+import { NotFoundComponent } from "./components/notFoundComponent/not-found/not-found.component";
+import { AuthGuard } from 'src/app/components/guards/auth.guard';
+import { LoginComponent } from "./components/login/login.component";
 
 const routes: Routes = [
-  {
-    path: "order",
-    component: OrderComponent, canActivate: [AuthGuard]
-  },
-  { path: "claim", component: ClaimComponent, canActivate: [AuthGuard]},
+  { path: "user", loadChildren:()=>import('./modules/user/user.module').then((m)=>m.UserModule) , canActivate: [AuthGuard]}, 
+  { path: "login", component: LoginComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'}, 
+
+  { path: "**", component: NotFoundComponent},
 ];
 
 @NgModule({
